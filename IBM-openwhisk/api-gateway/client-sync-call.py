ba32eba840dfe3298176192313c731d54d40967d
Kumar Satyam <kr.satyam.ind@gmail.com>
2017-10-25 00:29:57 -0400

    Update client-sync-call.py

--- IBM-openwhisk/api-gateway/client-sync-call.py
+++ IBM-openwhisk/api-gateway/client-sync-call.py
@@ -5,7 +5,7 @@ from datetime import datetime
 def call():
     for i in range(200):
         data = {"Id":"google-"+str(i),"name":"mountain view-"}
-        url="https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/970ac94052782aa2caab8c95f619a43fa7391fb985693bb142a27551f12a3842/function/myaction"
+        url="<give the URL of action>"
         res= requests.post(url=url,data=data) #res= requests.post(url=url,data=data)
         #print(res.text)
 
